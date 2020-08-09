04973ceb4875b126e596b48ff334e1e23c792a39
Samhitha <samhithagurramkonda@gmail.com>
2020-08-09 17:41:13 +0530

    second commit by x

--- Mavenjava/Mavenjava/src/test/java/qaclickacademy/RESTAPITest.java
+++ Mavenjava/Mavenjava/src/test/java/qaclickacademy/RESTAPITest.java
@@ -8,6 +8,8 @@ public class RESTAPITest {
 	public void postJira()
 	{
 		System.out.println("postJira");
+		System.out.println("postJira 2222");
+		System.out.println("postJira 3333");
 	}
 	
 	@Test
