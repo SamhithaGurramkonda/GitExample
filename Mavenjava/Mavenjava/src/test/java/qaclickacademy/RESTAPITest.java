d74f222c5afa5e76e5366dbf6ebaa5b4a9bade1a
Samhitha <samhithagurramkonda@gmail.com>
2020-08-09 19:00:24 +0530

    checking out merge conflicts

--- Mavenjava/Mavenjava/src/test/java/qaclickacademy/RESTAPITest.java
+++ Mavenjava/Mavenjava/src/test/java/qaclickacademy/RESTAPITest.java
@@ -25,13 +25,6 @@ public class RESTAPITest {
 	public void deleteTwitter()
 	{
 		System.out.println("deleteTwitter");	
-		System.out.println("deleteTwitter");
-
-		System.out.println("This is the change to be reflected only on BRANCH1.");
-		System.out.println("222222 BRANCH1.");
-		
-		System.out.println("Fisrt commit by X");
-		System.out.println("First commit by X");
 		
 	}
 }
